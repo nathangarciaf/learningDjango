72fb8bb51567ea075b88d479e23224b1d6053b01
nathangarciaf <nathangarfreitas@gmail.com>
2024-05-02 01:34:26 -0300

    tut 2.3

--- mytestsite/catalog/models.py
+++ mytestsite/catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class MyModelName(models.Model):
+    """Uma típica classe definindo um modelo, derivada da classe Model."""
+
+    # Campos
+    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
+    ...
+
+    # Metadados
+    class Meta:
+        ordering = ['-my_field_name']
+
+    # Métodos
+    def get_absolute_url(self):
+        """Retorna a url para acessar uma instancia específica de MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """ String para representar o objeto MyModelName (no site Admin)."""
+        return self.my_field_name
